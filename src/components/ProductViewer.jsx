0a49ad64a462f1f814f22a358ac58e145f1eaa2f
kharelabhishek <kharela86@gmail.com>
2025-11-15 13:53:56 -0600

    Implement 3D product viewer section

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProdiuctViewer = () => {
+    return (
+        <section id="product-viewer">
+            <h2>Take a closer look</h2>
+
+            <div className="controls">
+                <p className="info"> MacbookPro 16'' in Space Black</p>
+            </div>
+        </section>
+    )
+}
+export default ProdiuctViewer
